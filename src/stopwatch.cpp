0e184acefcc3fc2d933d4d0e13a4710026b0ec19
Andrey Davydov <andrey.a.davydov@gmail.com>
2013-02-27 09:14:28 +0400

    stopwatch(): message when uncaught exception

--- src/stopwatch.cpp
+++ src/stopwatch.cpp
@@ -2,6 +2,8 @@
 #include "logger/stopwatch.h"
 #include "logger/debug_stream.h"
 
+#include <exception>
+
 namespace util
 {
     stopwatch::stopwatch( const char * message )
@@ -16,16 +18,14 @@ namespace util
     stopwatch::stopwatch( std::string const & message ) 
         : stopwatch(message.c_str()) 
     {}
-    
-    stopwatch::stopwatch( boost::format const & message )
-        : stopwatch(message.str())
-    {}
 
     stopwatch::~stopwatch()
     {
         using namespace boost::chrono;
 
         clock_t::time_point finish = clock_t::now();
+        if (std::uncaught_exception())
+            debug_stream() << "exception was thrown"; 
 		debug_stream::dec_indent();
         auto duration = duration_cast<milliseconds>(finish - start);
         debug_stream() << (duration.count() / 1000.0) << " sec";
